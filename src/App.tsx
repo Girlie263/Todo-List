0f70ce5d57ee94cf0a72118e9c151c58a01583be
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2025-05-29 23:29:07 +0200

    Added some functionality to the input  and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
+import { useState, type  ChangeEvent,} from "react"
+import type { ITask } from "./Interfaces";
+
+const  App =() => {
+
+  const [task, setTask] = useState<string>('');
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todoList, setTodoList] = useState<ITask[]>([])
 
 
-function App() {
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>): void => {
+    if(e.target.value === 'task') {
+      setTask(e.target.value)
+    }else {
+      setDeadline(Number(e.target.value))
+    }
+  };
+
+  const handleClick = (): void => {
+    const newTask: ITask = {
+      taskName: task,
+      deadline:deadline
+    };
+    setTodoList([...todoList, newTask])
+  }
+
+
+
 
   return (
     <>
     <h1>Todo-List</h1>
     <div>
-      <input type="text"  placeholder="Enter Task"/>
-      <input type="number" placeholder="Enter deadline(in days)" />
+      <input type="text"  name="task"  placeholder="Enter Task" onChange={handleChange}/>
+      <input type="number"name="deadline" placeholder="Enter deadline(in days)" onChange={handleChange} />
     </div>
-    <button>Add Task</button>
+    <button onClick={handleClick}>Add Task</button>
     </>
   )
 }
