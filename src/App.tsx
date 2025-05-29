2c1ed6768817bf758b28e6d3584f5a902f2500fc
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2025-05-29 22:55:11 +0200

    wrote a scatch code of what to expect from the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Todo-List</h1>
+    <div>
+      <input type="text"  placeholder="Enter Task"/>
+      <input type="number" placeholder="Enter deadline(in days)" />
+    </div>
+    <button>Add Task</button>
+    </>
+  )
+}
+
+export default App
