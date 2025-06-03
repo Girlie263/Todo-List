7b19f118384e3d3d7c5ba4e7ccfe5ae17a0826ba
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2025-06-03 22:40:08 +0200

    managed to make the list appear on the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 
 import { dummyData } from "./Components/todos"
+import TodoItem from "./Components/TodoItem";
 
 function App() {
   return (
     <main className="bg-gray-100 h-screen">
       <h1 className="font-bold text-3xl text-center">Todos</h1>
       <div className="max-w-lg mx-auto">
-        <div className="space-y-2">
+        <div className="space-y-2 shadow-md rounded-md bg-white p-4 mt-4 text-center">
           {dummyData.map(todo => (
-            <p key={todo.id} className="text-center">
-              {todo.title}</p>
+            <TodoItem todo={todo} />
+            
           ))}
         </div>
       </div>
--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -1,6 +1,12 @@
-export default function TodoItem() {
-    return ( 
-        <div>Todo Item</div>
-    )
+import {type Todo } from '../types/todo';
 
+interface TodoItemProps {
+    todo: Todo;
+}
+
+
+export default function TodoItem({todo}:TodoItemProps) {
+    return (
+        <div>{todo.title}</div>
+    )
 }
