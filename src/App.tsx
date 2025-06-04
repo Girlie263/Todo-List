87721d072a7111e2bf74aa2d98cfe65c246efe53
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2025-06-04 20:50:14 +0200

    added functionality to mark through the competed todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 
 import { dummyData } from "./Components/todos"
 import TodoItem from "./Components/TodoItem";
+import { useState } from "react";
 
 function App() {
+  const [todos,setTodos]= useState(dummyData);
+
+  function handleCompletedTodo(id: number, completed: boolean) {
+    setTodos((prevTodos) => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: completed }
+        } else {
+          return todo
+        }
+      })
+    })
+  }
+
+
   return (
     <main className="bg-gray-100 h-screen">
       <h1 className="font-bold text-3xl text-center">Todos</h1>
       <div className="max-w-lg mx-auto">
         <div className="space-y-2 shadow-md rounded-md bg-white p-4 mt-4 text-center">
-          {dummyData.map(todo => (
-            <TodoItem todo={todo} />
+          {todos.map(todo => (
+            <TodoItem todo={todo}
+            key ={todo.id}
+            onCompletedTodo={handleCompletedTodo}
+
+             />
             
           ))}
         </div>
--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -2,11 +2,20 @@ import {type Todo } from '../types/todo';
 
 interface TodoItemProps {
     todo: Todo;
+    onCompletedTodo: (id: number, completed: boolean) => void;
 }
 
 
-export default function TodoItem({todo}:TodoItemProps) {
+export default function TodoItem({todo, onCompletedTodo}:TodoItemProps) {
     return (
-        <div>{todo.title}</div>
+        <div>
+            <label>
+                <input type="checkbox"
+                checked = {todo.completed}
+                onChange={(e) => onCompletedTodo(todo.id, e.target.checked)}
+                 />
+                 <span className={ todo.completed ? "line-through gray-600 text-gray-300": ""}>{todo.title}</span>
+            </label>
+        </div>
     )
 }
